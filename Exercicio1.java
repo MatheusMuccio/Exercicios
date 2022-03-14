4e2e7c111543f4fd654c3defdd9a7e0c9cbf7c23
Matheus Muccio <matheus_muccio@outlook.com>
2022-03-14 17:39:47 +0000

    Atualizando

--- Exercicio1.java
+++ Exercicio1.java
@@ -3,7 +3,7 @@ public class Exercicio1 {
 
     public static void main(String[] args) {
 
-        for (int x = 0; x <= 300; x++){
+        for (int x = 150; x <= 300; x++){
             System.out.println("Valor: " + x);
         }
     }
